c87bb3b37f194a712980c28eb93daef9801cee6f
KNME <g.giorgobiani@gtu.ge>
2018-02-13 17:28:16 +0400

    Add files via upload

--- TEST232/modules/window.c
+++ TEST232/modules/window.c
@@ -1,9 +1,11 @@
-// минимальный код для создания окна при помощи библиотеки ncurses.h
-
 #include <ncurses.h>
+#include "window.h"
 
- 
-int main()
+
+int get_size_of_win(int max_x, int max_y);
+
+
+void about_win(int max_x, int max_y)
 
 {
 
@@ -12,7 +14,7 @@ int main()
     
 	WINDOW *window_main;
    
-	window_main = newwin( 20, 20, 0, 0 );
+	window_main = newwin( 40, 40, 0, 0 );
 	box(window_main, 0, 0);
 
 	refresh();
@@ -24,8 +26,60 @@ int main()
 	delwin(window_main);
 	endwin();
  
-return 0;
- 
 }
 
+
+
+
+void login_win(int max_x, int max_y)
+
+{
+
+	getch();
+
+	delwin(login_win);
+	endwin();
+
+}
+
+
+
+
+void main_win(int max_x, int max_y)
+
+{
+
+	getch();
+
+	delwin(main_win);
+	endwin();
+
+}
+
+
+
+
+void exit_win(int max_x, int max_y)
+
+{
+
+	getch();
+
+	delwin(exit_win);
+	endwin();
+
+}
+
+
+
+
+int get_size_of_win(int max_x, int max_y)
+
+{
+
+        initscr();
+        getmaxyx(stdscr, max_x, max_y);
+        endwin();
+
+}
 
---
+++ TEST232/modules/window.h
@@ -0,0 +1,19 @@
+#ifdef __WINDOW_H
+#define __WINDOW_H
+
+ifdef __cplusplus
+extern "C" {
+#enif
+
+void get_size_of_win(int max_x, max_y);
+void about_win(int max_x, max_y);
+void login_win(int max_x, max_y);
+void main_win(int max_x, max_y);
+void exit_win(int max_x, max_y);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
